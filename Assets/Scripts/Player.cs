01bce63f1f7e62cf0d2f293e1a0619aabfb9660c
Erik <edamestica@soincon.es>
2020-11-07 10:48:55 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    private int speed = 30;
+    private float jumpPower = 999f;
+    private float saltando;
+    private Rigidbody2D rb2d;
+    private float distancia;
+    private LayerMask whatIsLadder;
+    private bool escalando;
+    private float jumpForce = 7;
+
+    Animator animador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        saltando = 0f;
+        float x = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x, 0) * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            saltando = 1f;
+        }
+
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            animador.SetBool("agachado", true);
+        }
+        if (!Input.GetKeyDown("left ctrl"))
+        {
+            animador.SetBool("agachado", false);
+        }
+
+        animador.SetFloat("salto", saltando);
+        animador.SetFloat("andar", x);
+
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distancia, whatIsLadder);
+
+        if (hitInfo.collider != null)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                escalando = true;
+            }
+
+        }
+        else
+        {
+            escalando = false;
+        }
+        if (escalando)
+        {
+            float y = Input.GetAxisRaw("Vertical");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, y) * speed;
+            rb2d.gravityScale = 0;
+
+        }
+        else
+        {
+            rb2d.gravityScale = 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "escena2")
+        {
+            SceneManager.LoadScene("escena2");
+        }
+        if (collision.gameObject.name == "escena1")
+        {
+            SceneManager.LoadScene("escena1");
+        }
+    }
+}
---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    public bool bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    //private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void FixedUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //transform.position = player.transform.position + offset;
+
+        if (bounds) {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+        if (player.transform.position.x == -3.4) {
+            transform.position = minCameraPos;
+        }
+        if (player.transform.position.x == 9)
+        {
+            transform.position = minCameraPos;
+        }
+    }
+}
